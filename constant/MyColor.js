7ca770cb1f03573beb5c7880be61260a8f7e7b73
savic <savic@tech.co>
2022-09-28 14:18:50 +0100

    From the start

---
+++ constant/MyColor.js
@@ -0,0 +1,10 @@
+export default {
+    textColor: 'white',
+    textColorLighter: '#e8e4c9',
+    lineBreakColor: '#F5F5F5',
+    iconContainerColor: '#e8d21d',
+    profilePhotoContainer: '#039fbe',
+    buttonColor: '#cf1578',
+    pressableFeedback: '#b20238',
+    iconObjectColor:'#002851'
+}
