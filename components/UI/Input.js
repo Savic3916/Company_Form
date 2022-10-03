d5afa7b186576d1fe700023a34b803103c94eb96
savic <savic@tech.co>
2022-10-03 12:17:24 +0100

    Add an empty companyTab.js and Input.js

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function CompanyTab() {
+export default function Input() {
   return (
     <View>
-      <Text>CompanyTab</Text>
+      <Text>Input</Text>
     </View>
   )
 }
