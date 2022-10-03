38dab8677efdabe76e8cbb9c43425658fb563a05
savic <savic@tech.co>
2022-10-03 13:28:56 +0100

    Create a textInput and add the first input on the CompanyTab.js

--- components/Tab/CompanyTab.js
+++ components/Tab/CompanyTab.js
@@ -1,12 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Input from '../UI/Input'
 
 export default function CompanyTab() {
   return (
-    <View>
-      <Text>CompanyTab</Text>
+    <View style={styles.companyTab}>
+      <Text style={styles.text}> Name </Text>
+      <Input prop={{placeholder: 'Name of company'}}/>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  companyTab: {
+    marginVertical: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 13,
+  }
+})
--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput } from 'react-native'
 import React from 'react'
 
-export default function Input() {
+export default function Input({prop}) {
   return (
-    <View>
-      <Text>Input</Text>
-    </View>
+      <TextInput style={styles.textInput} {...prop}/>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textInput: {
+    padding: 6,
+    borderRadius: 6,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+    fontSize: 12,
+    marginTop: 5,
+  }
+})
